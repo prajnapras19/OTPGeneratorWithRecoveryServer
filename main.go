f3709daf873aa75f2454f624c18e3d6ab3d46350
Prajna Prasetya <prajnapras19@gmail.com>
2023-04-22 09:51:42 +0700

    feat: add automigrate

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/prajnapras19/otpgeneratorwithrecoveryserver/client/mysql"
+	"github.com/prajnapras19/otpgeneratorwithrecoveryserver/config"
+	"github.com/prajnapras19/otpgeneratorwithrecoveryserver/sharedsecret"
+)
+
 func main() {
-	// TODO
+	// config
+	cfg := config.Get()
+
+	// database
+	mySQLService := mysql.NewService(cfg.MySQLConfig)
+
+	// auto migrate
+	mySQLService.GetDB().AutoMigrate(&sharedsecret.SharedSecret{})
+
+	// repository
+	sharedSecretRepository := sharedsecret.NewRepository(mySQLService.GetDB())
+
+	// handler
+	sharedSecretHandler := sharedsecret.NewHandler(sharedSecretRepository)
+
+	r := gin.Default()
+	r.POST("/insert", sharedSecretHandler.Insert)
+	r.GET("/get/:client_id", sharedSecretHandler.Get)
+
+	r.Run(":" + cfg.RESTPort)
 }
